f8b07ad091df8e964324dc69bfa265b5ac1f1fd2
Ben Christel <ninguna@nada.com>
2017-04-01 11:21:22 -0700

    Add LineBuffer with a few basic tests
    - The ultimate purpose of the LineBuffer is to represent a single
      line of output on the screen. It will enable formatting portions
      of the screen with different text colors, formatting, and
      highlighting/background colors.

---
+++ src/LineBuffer.js
@@ -0,0 +1,17 @@
+var _64_SPACES
+  = '                                '
+  + '                                '
+
+function makeScreenWidth(s) {
+  return (s + _64_SPACES.slice(s.length)).slice(0, 64)
+}
+
+function LineBuffer(text) {
+  return {
+    toHTML: toHTML
+  }
+
+  function toHTML() {
+    return makeScreenWidth(text)
+  }
+}
---
+++ test/test-LineBuffer.js
@@ -0,0 +1,35 @@
+describe('LineBuffer', function() {
+  it('pads a short line to 64 chars', function() {
+    var input = 'asdf'
+    var expected
+      = 'asdf                            '
+      + '                                '
+
+    expect(LineBuffer(input).toHTML()).toBe(expected)
+    expect(LineBuffer(input).toHTML().length).toBe(64)
+  })
+
+  it('truncates a long line to 64 chars', function() {
+    var input
+      = '1234567890abcdef1234567890abcdef'
+      + '1234567890abcdef1234567890abcdef'
+      + 'off the screen'
+
+    var expected
+      = '1234567890abcdef1234567890abcdef'
+      + '1234567890abcdef1234567890abcdef'
+
+    expect(LineBuffer(input).toHTML()).toBe(expected)
+    expect(LineBuffer(input).toHTML().length).toBe(64)
+  })
+
+  it('outputs a line of 64 spaces given an empty string', function() {
+    var input = ''
+    var expected
+      = '                                '
+      + '                                '
+
+    expect(LineBuffer(input).toHTML()).toBe(expected)
+    expect(LineBuffer(input).toHTML().length).toBe(64)
+  })
+})
